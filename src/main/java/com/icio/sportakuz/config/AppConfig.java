c260fe9945b2ef39b590b9ed8dfcaa919a748df2
Zalewa <zalewski.ba@gmail.com>
2025-11-01 12:49:57 +0100

    SPORTAKUZ-82: Implement Thymeleaf & provide folder structure for resources

--- src/main/java/com/icio/sportakuz/config/AppConfig.java
+++ src/main/java/com/icio/sportakuz/config/AppConfig.java
@@ -4,18 +4,49 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.icio")
 public class AppConfig {
 
-    @Bean
-    public InternalResourceViewResolver viewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/views/"); // folder where JSPs are located
-        resolver.setSuffix(".jsp");            // JSP extension
-        return resolver;
-    }
+	/*
+	* Allows Thymeleaf to find and interpret templates
+	* */
+	@Bean
+	public SpringResourceTemplateResolver templateResolver() {
+		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+		templateResolver.setPrefix("classpath:/templates/"); // "classpath:" - compiled resources (src/main/resources/)
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		templateResolver.setCharacterEncoding("UTF-8");
+		templateResolver.setCacheable(false); // templates reloaded on every request (useful in dev)
+		return templateResolver;
+	}
+
+	/*
+	* Transforms Thymeleaf templates into HTML
+	* */
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver());
+		templateEngine.setEnableSpringELCompiler(true);
+		return templateEngine;
+	}
+
+	/*
+	* Provides communication between Spring MVC & Thymeleaf
+	* */
+	@Bean
+	public ThymeleafViewResolver viewResolver() {
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(templateEngine());
+		viewResolver.setCharacterEncoding("UTF-8");
+		return viewResolver;
+	}
 }
--- src/main/java/com/icio/sportakuz/controllers/HomePageController.java
+++ src/main/java/com/icio/sportakuz/controllers/HomePageController.java
@@ -3,14 +3,18 @@ package com.icio.sportakuz.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomePageController {
 
-    @GetMapping("/hello")
-    public String hello(Model model) {
-        model.addAttribute("message", "Hello from JSP view!");
-        return "HomePageView"; // resolves to /WEB-INF/views/HomePageView.jsp
-    }
+	@GetMapping("/")
+	public String index() {
+		return "index"; // /resources/templates/index.html
+	}
+
+	@GetMapping("/hello")
+	public String hello(Model model) {
+		model.addAttribute("message", "messagemessagemessagemessagemessage");
+		return "HomePageView"; // /resources/templates/HomePageView.html
+	}
 }
