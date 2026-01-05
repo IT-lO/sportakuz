9f4ea24896de8ba8bfe75bb578430e5543c90ff9
przewsr <przewsr@gmail.com>
2026-01-05 11:39:00 +0100

    SPORTAKUZ-9 Logowanie - rejestracja - captcha (#12)
    * Add actual login/register functionality
    
    * Add reCaptcha
    
    * Improve Register form
    
    * Add Activity/Room data initialization
    
    * Add and fix Captcha on login
    
    ---------
    
    Co-authored-by: Adsekiii <112149@g.elearn.uz.zgora.pl>

--- src/main/java/com/icio/sportakuz/dto/UserRegister.java
+++ src/main/java/com/icio/sportakuz/dto/UserRegister.java
@@ -1,13 +1,31 @@
 package com.icio.sportakuz.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserRegister {
+
+    @NotEmpty(message = "Imię jest wymagane")
     private String firstName;
+
+    @NotEmpty(message = "Nazwisko jest wymagane")
     private String lastName;
+
+    @NotEmpty(message = "Email jest wymagany")
+    @Email(message = "Nieprawidłowy format email")
     private String email;
+
+    @NotEmpty(message = "Hasło jest wymagane")
+    @Size(min = 8, message = "Hasło musi mieć minimum 8 znaków")
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!_.*-]).{8,}$",
+            message = "Hasło musi zawierać małą i wielką literę, cyfrę oraz znak specjalny"
+    )
     private String password;
 }
